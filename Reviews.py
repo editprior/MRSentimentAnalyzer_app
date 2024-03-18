a6e3fe878dc1fc17a6a98781b1e14d5924fe34b9
Ekaterina ROZHKOVA <146826298+editprior@users.noreply.github.com>
2024-03-18 18:32:52 +0100

    Update Reviews.py

--- Reviews.py
+++ Reviews.py
@@ -84,9 +84,8 @@ def create_colored_review(review, word_contribution):
 
 # Function to create word cloud
 def create_word_cloud(word_contribution):
-    wordcloud_dict = {word: abs(contribution) for word, contribution in word_contribution} # Create a dictionary of words and their contributions
-    wordcloud = WordCloud(width=1000, height=800, background_color ='white').generate_from_frequencies(wordcloud_dict) # Generate a word cloud from the dictionary
-    # Plot the word cloud
+    wordcloud_dict = {word: abs(contribution) for word, contribution in word_contribution}
+    wordcloud = WordCloud(width=1000, height=800, background_color ='white').generate_from_frequencies(wordcloud_dict)
     plt.figure(figsize=(15, 12))
     plt.imshow(wordcloud, interpolation='bilinear')
     plt.axis('off')
